b5b83b5efd9053979a1bebfcaa9b3d45b095f016
Eugene Tulusha <tulusheg@gmail.com>
2016-03-21 16:18:19 +0200

    Add public functions of the kit

---
+++ GifMagicKit/GifMagic.swift
@@ -0,0 +1,15 @@
+//
+//  GifMagic.swift
+//  GifMagicKit
+//
+//  Created by Eugene on 3/21/16.
+//  Copyright © 2016 FocusedGenius. All rights reserved.
+//
+
+public func GifMagic(videoURL videoURL:NSURL, loopCount: Int, completion: (NSURL) -> ()) {
+    
+}
+
+public func GifMagic(videoURL videoURL:NSURL, frameCount: Int, delayTime: Int, loopCount: Int, completion: (NSURL) -> ()) {
+    
+}
