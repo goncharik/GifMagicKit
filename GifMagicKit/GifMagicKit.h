154a731fbca62a8ca8cc5a1413e13c6e6ab9a18c
Eugene Tulusha <tulusheg@gmail.com>
2016-03-21 14:15:28 +0200

    Initial setup

---
+++ GifMagicKit/GifMagicKit.h
@@ -0,0 +1,19 @@
+//
+//  GifMagicKit.h
+//  GifMagicKit
+//
+//  Created by Eugene on 3/21/16.
+//  Copyright © 2016 FocusedGenius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GifMagicKit.
+FOUNDATION_EXPORT double GifMagicKitVersionNumber;
+
+//! Project version string for GifMagicKit.
+FOUNDATION_EXPORT const unsigned char GifMagicKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GifMagicKit/PublicHeader.h>
+
+
